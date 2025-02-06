a1cff0ea9708ef593ec17620e4fbf68efd9be576
Tamam Krajem <127201245+Tamamkarim@users.noreply.github.com>
2025-02-06 23:01:55 +0200

    Update TK 7.2.py

--- TK 7.2.py
+++ TK 7.2.py
@@ -1,28 +1,15 @@
-def 
-    tulosta_vuodenaika(kuukausi_numero):
-    vuodenajat = {
-        (12, 1, 2): "Talvi",
-        (3, 4, 5): "Kevät",
-        (6, 7, 8): "Kesä",
-        (9, 10, 11): "Syksy"
-    }
+ nimet = set()
 
-    for kuukaudet, vuodenaika in vuodenajat.items():
-        if kuukausi_numero in kuukaudet:
-            print(f"Kuukausi {kuukausi_numero} on {vuodenaika}.")
-            return
+while True:
+    nimi = input("Syötä nimi (tyhjä merkkijono lopettaa): ")
+    if nimi == "":
+        break
+    if nimi in nimet:
+        print("Aiemmin syötetty nimi")
+    else:
+        print("Uusi nimi")
+        nimet.add(nimi)
 
-    print("Virheellinen kuukausi numero.")
-
-if __name__ == "__main__":
-    while True:
-        try:
-            kuukausi_numero = int(input("Anna kuukauden numero (1-12): "))
-            if 1 <= kuukausi_numero <= 12:
-                break
-            else:
-                print("Kuukausi numeron on oltava välillä 1-12.")
-        except ValueError:
-            print("Virheellinen syöte. Anna numero.")
-
-    tulosta_vuodenaika(kuukausi_numero)
+print("\nSyötetyt nimet:")
+for nimi in nimet:
+    print(nimi)
